673fce693779000182bf4383d309c9a7709d227c
riosengineer <36534747+riosengineer@users.noreply.github.com>
2021-12-21 12:06:59 +0000

    Create proxy-addresses.ps1

---
+++ proxy-addresses.ps1
@@ -0,0 +1,11 @@
+# PS Script to add Proxy Addresses to AD Users via CSV list 
+Import-Module ActiveDirectory
+# Specify Path of where the addresses are located. Must be a CSV and have SamAccountName and ProxyAddresses as column headings
+$path = 'C:\users\dan.rios\desktop\adr.csv'
+$csv=Import-Csv $path
+
+#Script to add the addresses from each csv line
+foreach($User in $csv)
+{
+Foreach($address in $user.ProxyAddresses) { Set-ADUser $User.SamAccountName -Add @{ProxyAddresses=$Address}}
+}
