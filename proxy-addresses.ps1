37f1a6684c5841aacaba1c65570de226b538a59c
Dan Rios <36534747+riosengineer@users.noreply.github.com>
2023-09-05 16:35:19 +0100

    Update proxy-addresses.ps1
    Correcting path var

--- proxy-addresses.ps1
+++ proxy-addresses.ps1
@@ -1,7 +1,7 @@
 # PS Script to add Proxy Addresses to AD Users via CSV list 
 Import-Module ActiveDirectory
 # Specify Path of where the addresses are located. Must be a CSV and have SamAccountName and ProxyAddresses as column headings
-$path = Your path to.csv'
+$path = 'Your path to.csv'
 $csv=Import-Csv $path
 
 #Script to add the addresses from each csv line
